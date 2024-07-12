594b526b8485d7a9284289219fa10a86491429ac
Sideways-Sky <96632645+Sideways-Sky@users.noreply.github.com>
2024-07-11 18:06:52 -0600

    v1.1-0
    Added 1.21 support

--- src/main/java/net/sideways_sky/herdspanic/HerdsPanic.java
+++ src/main/java/net/sideways_sky/herdspanic/HerdsPanic.java
@@ -6,6 +6,7 @@ import net.minecraft.world.entity.ai.goal.PanicGoal;
 import net.minecraft.world.entity.ai.goal.WrappedGoal;
 import org.bukkit.Bukkit;
 import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.craftbukkit.entity.CraftEntity;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.EntityType;
 import org.bukkit.event.EventHandler;
@@ -13,19 +14,12 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
 public final class HerdsPanic extends JavaPlugin implements Listener {
-
-    private final String CRAFTBUKKIT_PACKAGE = Bukkit.getServer().getClass().getPackage().getName();
-    public Class<?> cbClass(String clazz) throws ClassNotFoundException {
-        return Class.forName(CRAFTBUKKIT_PACKAGE + "." + clazz);
-    }
 
     @Override
     public void onEnable() {
@@ -42,20 +36,11 @@ public final class HerdsPanic extends JavaPlugin implements Listener {
             heards.add(type);
             speeds.put(type, complex.getDouble(key));
         }
-        try {
-            CraftEntityGetHandle = cbClass("entity.CraftEntity").getMethod("getHandle");
-        } catch (ClassNotFoundException | NoSuchMethodException e) {
-            throw new RuntimeException(e);
-        }
-    }
-    public static void consoleSend(String message){
-        Bukkit.getConsoleSender().sendMessage("[HeardsPanic] " + message);
     }
     public static int radius = 20;
     public static int delay = 0;
     public static Set<EntityType> heards = new HashSet<>();
     public static HashMap<EntityType, Double> speeds = new HashMap<>();
-    static Method CraftEntityGetHandle;
     @EventHandler
     public void onEntityDamageByEntity(EntityDamageByEntityEvent e){
         if(e.getDamager().getType() != EntityType.PLAYER || !heards.contains(e.getEntityType())){return;}
@@ -63,22 +48,18 @@ public final class HerdsPanic extends JavaPlugin implements Listener {
         Bukkit.getScheduler().runTaskLater(this, () -> {
             for (Entity entity : entities) {
                 if(entity.getType() != e.getEntityType()){continue;}
-                try {
-                    Mob mob = (Mob) CraftEntityGetHandle.invoke(entity);
-                    mob.goalSelector.getAvailableGoals().stream().filter(wrappedGoal -> wrappedGoal.getGoal() instanceof PanicGoal)
-                            .findFirst().ifPresentOrElse(WrappedGoal::start, () -> {
-                                if(!speeds.containsKey(e.getEntityType())){
-                                    consoleSend("Entity type " + e.getEntityType() + " has no panic Goal and no configuration to add one! (Use complex-heards)");
-                                    return;
-                                }
-                                double speed = speeds.get(e.getEntityType());
-                                PanicGoal goal = new PanicGoal((PathfinderMob) mob, speed);
-                                mob.goalSelector.addGoal(1, goal);
-                                goal.start();
-                            });
-                } catch (InvocationTargetException | IllegalAccessException ex) {
-                    throw new RuntimeException(ex);
-                }
+                Mob mob = (Mob) ((CraftEntity) entity).getHandle();
+                mob.goalSelector.getAvailableGoals().stream().filter(wrappedGoal -> wrappedGoal.getGoal() instanceof PanicGoal)
+                        .findFirst().ifPresentOrElse(WrappedGoal::start, () -> {
+                            if(!speeds.containsKey(e.getEntityType())){
+                                getLogger().info("Entity type " + e.getEntityType() + " has no panic Goal and no configuration to add one! (Use complex-heards)");
+                                return;
+                            }
+                            double speed = speeds.get(e.getEntityType());
+                            PanicGoal goal = new PanicGoal((PathfinderMob) mob, speed);
+                            mob.goalSelector.addGoal(1, goal);
+                            goal.start();
+                        });
             }
         }, delay);
     }
